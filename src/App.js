57c2117486529a509cfd7127e4632872479aa1be
Edward Ganuelas <epganuelas@gmail.com>
2019-10-14 16:39:17 -0400

    add jumbotron

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from "./components/navbar/navbar";
+import Jumbotron from "./components/jumbotron/jumbotron";
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-        <NavBar />
+        {/* <NavBar /> */}
+        <Jumbotron title="EightRayedSun" />
     </div>
   );
 }
---
+++ src/components/jumbotron/jumbotron.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Jumbotron(props) {
+  return (
+    <div className="container-fluid header jumbotron">
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <h1>{props.title}</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
