5b4ce9bcc3bcac3b9b77253508f6b62aa1514d72
Edward Ganuelas <epganuelas@gmail.com>
2020-01-10 22:40:36 -0500

    blog updates

--- src/views/Blog/blog.js
+++ src/views/Blog/blog.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import client from '../../directus';
+import _ from 'lodash';
 import './blog.scss';
 
-export default () => {
-    return (
-        <>
+class Blog extends React.Component{
+    constructor(props) {
+        super(props);
+        this.blogPosts = null;
+        this.savedPost = null;
+    }
+    componentDidMount() {
+        this.getPosts();
+    }
+    async getPosts() {
+        if (_.isObject(this.savedPost)) {
+            return;
+        }
+        const response = await client.getItems('blog', {
+            filter: {
+                blog_type: "tech"
+            }
+        });
+        console.log(response);
+        // this.savedPost = response.data;
+    }
+    render() {
+        return (
+            <>
             <div className="container-fluid blog">
                 
             </div>
-        </>
-    );
+            </>
+        );
+    }
 }
+export default Blog;
