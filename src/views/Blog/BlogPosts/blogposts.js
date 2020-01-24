9918aad6f83357d9c38a8e560516fea191c784ff
Edward Paulo Ganuelas <epganuelas@gmail.com>
2020-01-23 20:28:47 -0500

    add moment, format date

--- src/views/Blog/BlogPosts/blogposts.js
+++ src/views/Blog/BlogPosts/blogposts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './blogposts.scss';
+import moment from 'moment';
 
 export default (props) => {
     const blogPosts = props.blogPosts;
@@ -7,6 +8,7 @@ export default (props) => {
             return ( 
                 <div className="col-12 blog-post" key={post.id}>
                     <h2>{post.title}</h2>
+                    <p className="published-date">{moment(post.publish_date, 'YYYY-MM-DD').format('MMM, DD, YYYY')}</p>
                     { post.excerpt &&
                         <p>{post.excerpt}</p>
                     }
