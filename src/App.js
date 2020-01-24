66af7c8cffd6e9bbfd92702d8ecc6175063bcf7d
Edward Paulo Ganuelas <epganuelas@gmail.com>
2020-01-23 20:07:17 -0500

    update nav, spacing

--- src/App.js
+++ src/App.js
@@ -5,34 +5,34 @@ import About from "./views/About/about";
 import Blog from "./views/Blog/blog";
 import Footer from "./components/footer/footer";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
+    BrowserRouter as Router,
+    Switch,
+    Route,
 } from "react-router-dom";
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App" id="App">
-        <Router>
-            <NavBar />
-            <div className="wrapper" id="wrapper">
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route exact path="/about">
-                        <About />
-                    </Route>
-                    <Route exact path="/blog">
-                        <Blog />
-                    </Route>
-                </Switch>
-            </div>
-        <Footer />
-        </Router>
-    </div>
-  );
+    return (
+        <div className="App" id="App">
+            <Router>
+                <NavBar />
+                <div className="wrapper" id="wrapper">
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route exact path="/about">
+                            <About />
+                        </Route>
+                        <Route exact path="/blog">
+                            <Blog />
+                        </Route>
+                    </Switch>
+                </div>
+            <Footer />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,34 +4,34 @@ import { slide as Menu } from 'react-burger-menu'
 import './navbar.scss';
 
 export default () => {
-  return (
-<>
-<Menu right  pageWrapId={ "wrapper" } outerContainerId={ "App" }>
-    <Link to="/" className="menu-item">Home</Link>
-    <a href="/documents/Edward_Ganuelas_Resume_08_12_2019.pdf" className="menu-item" target="_blank" rel="noopener noreferrer">Resume</a>
-    <a href="https://blog.eightrayedsun.com/" className="menu-item">Blog</a>
-    <a href="https://ca.linkedin.com/in/epganuelas" className="menu-item" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-    <a href="http://pokedex.eightrayedsun.com" className="menu-item" target="_blank" rel="noopener noreferrer">Pokedex Project</a>
-    <Link to="/about" className="menu-item">About</Link>
-    <a className="button is-primary is-inverted menu-item" href="https://www.github.com/edward-ganuelas" target="_blank" rel="noopener noreferrer">
-        <span className="icon">
-        <i className="fab fa-github"></i>
-        </span>
-        <span>
-        Github
-        </span>
-    </a>
-</Menu>
-<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div className="container">
-        <div className="navbar-header">
-            <Link to="/">
-                <img src="/images/eight.svg" width="32px" height="32px" alt="EightRayedSun" />
-            </Link>
-        </div>
-    </div>
-</nav>
-</>
-  );
+    return (
+            <>
+            <Menu right  pageWrapId={ "wrapper" } outerContainerId={ "App" }>
+                <Link to="/" className="menu-item">Home</Link>
+                <a href="/documents/Edward_Ganuelas_Resume_08_12_2019.pdf" className="menu-item" target="_blank" rel="noopener noreferrer">Resume</a>
+                <a href="/blog" className="menu-item">Blog</a>
+                <a href="https://ca.linkedin.com/in/epganuelas" className="menu-item" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                <a href="http://pokedex.eightrayedsun.com" className="menu-item" target="_blank" rel="noopener noreferrer">Pokedex Project</a>
+                <Link to="/about" className="menu-item">About</Link>
+                <a className="button is-primary is-inverted menu-item" href="https://www.github.com/edward-ganuelas" target="_blank" rel="noopener noreferrer">
+                    <span className="icon">
+                    <i className="fab fa-github"></i>
+                    </span>
+                    <span>
+                    Github
+                    </span>
+                </a>
+            </Menu>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container">
+                    <div className="navbar-header">
+                        <Link to="/">
+                            <img src="/images/eight.svg" width="32px" height="32px" alt="EightRayedSun" />
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+            </>
+    );
 }
 
