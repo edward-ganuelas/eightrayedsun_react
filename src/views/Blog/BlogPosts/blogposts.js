65e1612adbdbfe934581c6259b46103482a084e0
Edward Paulo Ganuelas <epganuelas@gmail.com>
2020-01-24 20:04:24 -0500

    render tags

--- src/views/Blog/BlogPosts/blogposts.js
+++ src/views/Blog/BlogPosts/blogposts.js
@@ -9,6 +9,10 @@ export default (props) => {
                 <div className="col-12 blog-post" key={post.id}>
                     <h2>{post.title}</h2>
                     <p className="published-date">{moment(post.publish_date, 'YYYY-MM-DD').format('MMM, DD, YYYY')}</p>
+                    {
+                        post.tags && 
+                        <p className="tags">{post.tags.join(', ')}</p>
+                    }
                     { post.excerpt &&
                         <p>{post.excerpt}</p>
                     }
